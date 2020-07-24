ced1b4d2b72748ebcfb7d43d84faba5e5a4edcd0
asafonov <me@asafonov.org>
2020-07-24 12:45:01 +0300

    Added highscore

--- view/score.js
+++ view/score.js
@@ -2,15 +2,33 @@ class ScoreView {
 
   constructor() {
     this.element = document.querySelector('div.scores span');
+    this.highscoreElement = document.querySelector('div.high span');
     asafonov.messageBus.subscribe(asafonov.events.SCORES_UPDATED, this, 'onScoresUpdated');
+    asafonov.messageBus.subscribe(asafonov.events.NEW_HIGHSCORE, this, 'onNewHighscore');
+    this.displayHighscore();
   }
 
   onScoresUpdated (eventData) {
     this.displayScore(eventData.scores);
   }
 
+  onNewHighscore (eventData) {
+    this.element.parentNode.classList.add('new_high');
+  }
+
   displayScore (score) {
     this.element.innerHTML = score;
+  }
+
+  displayHighscore() {
+    this.highscoreElement.innerHTML = asafonov.score.getHighScore() || "0";
+  }
+
+  destroy() {
+    this.element = null;
+    this.highscoreElement = null;
+    asafonov.messageBus.unsubscribe(asafonov.events.SCORES_UPDATED, this, 'onScoresUpdated');
+    asafonov.messageBus.unsubscribe(asafonov.events.NEW_HIGHSCORE, this, 'onNewHighscore');
   }
 
 }
