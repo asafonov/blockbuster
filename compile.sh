2deaa2237bf7bb296076107b9657ac7496e06f16
asafonov <me@asafonov.org>
2020-09-29 11:59:01 +0300

    Updated compile script

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
-> game.js
-for i in `ls models`; do cat models/$i >> game.js; done
-for i in `ls view`; do cat view/$i >> game.js; done
-cat globals.js >> game.js
-cat init.js >> game.js
+> main.js
+for i in `ls models`; do cat models/$i | grep -v ^[\n\s\t]*$ >> main.js; done
+for i in `ls controllers`; do cat controllers/$i | grep -v ^[\n\s\t]*$ >> main.js; done
+for i in `ls view`; do cat view/$i | grep -v ^[\n\s\t]*$ >> main.js; done
+cat globals.js >> main.js
+cat init.js >> main.js
